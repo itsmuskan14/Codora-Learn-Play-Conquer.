3b949674a02fbdf9687d1db3623f41e2235688d4
Muskan Bakshi <muskannb23hite@student.mes.ac.in>
2025-09-21 10:47:00 +0530

    Codora

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/Codora-Learn-Play-Conquer./", // 👈 replace 'codora' with your repo name
-  server: {
-    host: "::",
-    port: 8080,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
